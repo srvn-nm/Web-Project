e9cdf121d14683bad778d7120a9f19c57ac33eb6
Sarvin Nami <srvn0nm@gmail.com>
2024-02-01 22:49:23 +0330

    edit app.js

--- FrontEnd/web-project/src/App.js
+++ FrontEnd/web-project/src/App.js
@@ -1,44 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './Login';
-import ChatList from './ChatList';
-import ChatWindow from './ChatWindow';
-import UserProfile from './UserProfile';
+import RegisterPage from './RegisterPage';
+import LoginPage from './LoginPage';
+import ChatPage from './ChatPage';
+import { WebSocketProvider } from './WebSocketContext';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/chat-list" component={ChatList} />
-        <Route path="/chat/:id" component={ChatWindow} />
-        <Route path="/user/:id" component={UserProfile} />
-      </Switch>
+      <WebSocketProvider>
+        <Switch>
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/chat" component={ChatPage} />
+        </Switch>
+      </WebSocketProvider>
     </Router>
   );
 };
