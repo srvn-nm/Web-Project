3fbca9fbfd348c3f211b666b90dafad032448b96
Abtinz <abtinzandi@gmail.com>
2024-02-02 03:17:34 +0330

    urls

--- BackEnd/chat_project/user/urls.py
+++ BackEnd/chat_project/user/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 
 urlpatterns =  [
-    path("api/" , views.product_views, name = "product_view")
+    path("api/login/" , views.LoginView.as_view(), name = "login_view"),
+    path("api/register/" , views.SignupView.as_view(), name = "signup_view")
 ]
