40a5c452dd956f8c404806d36b4a45001cf4ca2b
Abtinz <abtinzandi@gmail.com>
2024-02-02 03:17:43 +0330

    for rest config

--- BackEnd/chat_project/chat_project/settings.py
+++ BackEnd/chat_project/chat_project/settings.py
@@ -37,13 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    "user",
+    "group",
+    "chat",
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
