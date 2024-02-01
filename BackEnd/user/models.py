daa4a885eccd2c5427e0e2c3896c2946b007b3f4
Abtinz <abtinzandi@gmail.com>
2024-02-01 19:17:13 +0330

    relational chat model

--- BackEnd/user/models.py
+++ BackEnd/user/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    identifier = models.IntegerChoices()
+    identifier = models.IntegerField() 
     lastname = models.CharField(max_length = 200)
     username = models.CharField(max_length = 200, unique = True)
-    fullname = models.CharField(max_length = 200)
+    firstname = models.CharField(max_length = 200)
     lastname = models.CharField(max_length = 200)
     phone = models.CharField(max_length = 40, unique = True)
     image = models.CharField(max_length = 350, unique = True)
