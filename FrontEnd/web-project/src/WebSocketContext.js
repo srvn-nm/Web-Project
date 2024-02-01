8412b0a6bafa2ebcf0263b72da65f174e7e0e886
Sarvin Nami <srvn0nm@gmail.com>
2024-02-01 23:00:09 +0330

    edit the web socket context

--- FrontEnd/web-project/src/WebSocketContext.js
+++ FrontEnd/web-project/src/WebSocketContext.js
@@ -8,22 +8,17 @@ export const useWebSocketContext = () => {
 };
 
 export const WebSocketProvider = ({ children }) => {
-  // اطلاعات اتصال WebSocket
-  const [socketUrl, setSocketUrl] = useState('ws://localhost:8000/socket');
-  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(socketUrl);
+  const [socketUrl] = useState('ws://localhost:8000/socket');
+  const { sendJsonMessage, readyState } = useWebSocket(socketUrl);
 
-  // ارسال پیام
   const sendMessage = (message) => {
     sendJsonMessage(message);
   };
 
-  // استفاده از useEffect برای رفتارهای جانبی در اتصال WebSocket
   useEffect(() => {
     if (readyState === ReadyState.OPEN) {
-      // اتصال برقرار شد
       console.log('WebSocket connected');
     } else if (readyState === ReadyState.CLOSED) {
-      // اتصال قطع شد
       console.log('WebSocket disconnected');
     }
   }, [readyState]);
